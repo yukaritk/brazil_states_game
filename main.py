334f038e6a39346ad48ee551e839acbb37ddf785
Tatiane Yukari <yukari.kawakami@hotmail.com>
2022-12-16 18:57:51 -0300

    update title

--- main.py
+++ main.py
@@ -13,14 +13,17 @@ turtle.shape(image)
 
 dashboard = Dashboard()
 data = pandas.read_csv("list_states.csv")
+score = 0
 
 game_on = True
 while game_on:
-    guess_states = screen.textinput(title="Guess of States", prompt="What's another state's names? ").lower()
+    guess_states = screen.textinput(title=f"{score}/27", prompt="What's another state's names? ").lower()
+
     for state in data.Estado:
         if state.lower() == guess_states:
             correct_state = data[data.Estado == state]
             dashboard.state_match(state, float(correct_state.x), float(correct_state.y))
+            score += 1
 
 
 screen.exitonclick()
