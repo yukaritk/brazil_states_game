fc79021ba97dd43a19c7d934d76c42eea6d22ca1
Tatiane Yukari <yukari.kawakami@hotmail.com>
2022-12-17 09:52:18 -0300

    fix bug states repeated

--- main.py
+++ main.py
@@ -13,17 +13,23 @@ turtle.shape(image)
 
 dashboard = Dashboard()
 data = pandas.read_csv("list_states.csv")
-score = 0
+all_states = data.Estado.to_list()
+score = []
+missing_states = []
 
 game_on = True
 while game_on:
-    guess_states = screen.textinput(title=f"{score}/27", prompt="What's another state's names? ").lower()
-
-    for state in data.Estado:
+    guess_states = screen.textinput(title=f"{len(score)}/27", prompt="What's another state's names? ").lower()
+    if guess_states == "exit":
+        for state in data.Estado:
+            if state not in score:
+                missing_states.append(state)
+            new_data = pandas.DataFrame(missing_states)
+            new_data.to_csv("states_to_learn.csv")
+        break
+    for state in all_states:
         if state.lower() == guess_states:
             correct_state = data[data.Estado == state]
             dashboard.state_match(state, float(correct_state.x), float(correct_state.y))
-            score += 1
-
-
-screen.exitonclick()
+            if state not in score:
+                score.append(state)
